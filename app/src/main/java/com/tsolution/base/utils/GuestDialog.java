05ff89495ce8bbae7aada598cce0140c37a3311f
Duy Tien <duytient31@gmail.com>
2020-11-10 16:08:34 +0700

    add check connectionInternet 2

--- app/src/main/java/com/tsolution/base/utils/GuestDialog.java
+++ app/src/main/java/com/tsolution/base/utils/GuestDialog.java
@@ -2,10 +2,13 @@ package com.tsolution.base.utils;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
@@ -49,11 +52,10 @@ public class GuestDialog extends Dialog implements View.OnClickListener {
         this.cancelable = cancelable;
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_guest);
+        setContentView(R.layout.dialog_guest_v2);
         initMainWindow();
         setCancelable(cancelable);
         initView();
@@ -62,43 +64,44 @@ public class GuestDialog extends Dialog implements View.OnClickListener {
     }
 
     private void initView() {
-        edUserName = findViewById(R.id.txtUserName);
-        edPassword = findViewById(R.id.txtPassword);
+//        edUserName = findViewById(R.id.txtUserName);
+//        edPassword = findViewById(R.id.txtPassword);
         buttonLogin = findViewById(R.id.btnLogin);
 
-        TextWatcher loginTextWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String userName = edUserName.getText().toString().trim();
-                String password = edPassword.getText().toString().trim();
-                buttonLogin.setEnabled(!userName.isEmpty() && !password.isEmpty());
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        };
-        edUserName.addTextChangedListener(loginTextWatcher);
-        edPassword.addTextChangedListener(loginTextWatcher);
+//        TextWatcher loginTextWatcher = new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                String userName = edUserName.getText().toString().trim();
+//                String password = edPassword.getText().toString().trim();
+//                buttonLogin.setEnabled(!userName.isEmpty() && !password.isEmpty());
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        };
+//        edUserName.addTextChangedListener(loginTextWatcher);
+//        edPassword.addTextChangedListener(loginTextWatcher);
     }
 
     private void initListener() {
 
         findViewById(R.id.btnLogin).setOnClickListener(this);
         findViewById(R.id.btnRegister).setOnClickListener(this);
-        findViewById(R.id.btnActivate).setOnClickListener(this);
+        //findViewById(R.id.btnActivate).setOnClickListener(this);
         findViewById(R.id.close).setOnClickListener(this);
     }
 
 
     private void initMainWindow() {
-        Objects.requireNonNull(getWindow()).setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        Objects.requireNonNull(getWindow()).setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
     }
 
     @Override
@@ -106,8 +109,8 @@ public class GuestDialog extends Dialog implements View.OnClickListener {
         if (view.getId() == R.id.btnLogin) {
             // Process login
             EventBus.getDefault().post(GuestActionDTO.builder()
-                    .userName(edUserName.getText().toString())
-                    .password(edPassword.getText().toString())
+//                    .userName(edUserName.getText().toString())
+//                    .password(edPassword.getText().toString())
                     .isProcessLogin(true)
                     .isProcessRegister(false)
                     .isActiveAccount(false)
@@ -121,18 +124,18 @@ public class GuestDialog extends Dialog implements View.OnClickListener {
                     .isActiveAccount(false)
                     .build());
             dismiss();
-        }
-        if (view.getId() == R.id.btnActivate) {
-            // Process Register
-            EventBus.getDefault().post(GuestActionDTO.builder()
-                    .isProcessLogin(false)
-                    .isProcessRegister(false)
-                    .isActiveAccount(true)
-                    .build());
-            dismiss();
         } else if (view.getId() == R.id.close) {
             dismiss();
         }
+//        if (view.getId() == R.id.btnActivate) {
+//            // Process Register
+//            EventBus.getDefault().post(GuestActionDTO.builder()
+//                    .isProcessLogin(false)
+//                    .isProcessRegister(false)
+//                    .isActiveAccount(true)
+//                    .build());
+//            dismiss();
+
     }
 
     @Override
